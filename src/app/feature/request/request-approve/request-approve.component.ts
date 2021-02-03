8d0b17910e0b9103ce7399976800886b85bb92c3
Derek Lee <dlee133@gmail.com>
2021-02-03 17:39:45 -0500

    tables for request A/R

--- src/app/feature/request/request-approve/request-approve.component.ts
+++ src/app/feature/request/request-approve/request-approve.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { LineItem } from 'src/app/model/line-item';
+import { LineItemService } from 'src/app/service/line-items.service';
 import { RequestService } from 'src/app/service/request.service';
 import { Request } from '../../../model/request.class';
 
@@ -11,17 +13,22 @@ import { Request } from '../../../model/request.class';
 })
 export class RequestApproveComponent implements OnInit {
   title = "Purchase Request Approve/Reject";
+  titleLi = "lines";
   request: Request = null ;
   requestId: number = 0;
+  lines: LineItem[] = [];
+  lineItem: LineItem = new LineItem();
   submitBtnTitle = "Save changes";
   
 
   constructor(private requestSvc: RequestService,
     private router: Router,
-    private route: ActivatedRoute) {
-  }
+    private route: ActivatedRoute,
+    private lineItemSvc: LineItemService,
+    ) { }
 
   ngOnInit(): void {
+    
     // get the id from the url
     this.route.params.subscribe(
       parms => {
@@ -32,6 +39,16 @@ export class RequestApproveComponent implements OnInit {
       resp => {
         this.request = resp as Request;
         console.log('Request', this.request);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+    // get line-items by request id
+    this.lineItemSvc.getAllLineItemsByRequestId(this.requestId).subscribe(
+      resp => {
+        this.lines = resp as LineItem[];
+        console.log('lineItem', this.lines);
       },
       err => {
         console.log(err);
